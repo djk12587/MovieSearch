f37d70cbbb8487ee1ce92993d27d46ce5fc4998b
Dan Koza <djk12587@gmail.com>
2018-08-18 19:18:55 -0500

    movie detail makes network call and updates itself besides the image

--- MovieSearch/Views/MovieDetailViewController.swift
+++ MovieSearch/Views/MovieDetailViewController.swift
@@ -10,11 +10,63 @@ import UIKit
 
 class MovieDetailViewController: UIViewController, SegueIdentifier
 {
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var moviePosterImageView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var movieRatingLabel: UILabel!
+    @IBOutlet weak var moviePlotLabel: UILabel!
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
         navigationItem.title = "Detail"
-        view.backgroundColor = .red
+
+        screenElements(shouldShow: false)
+
+        MovieApi.GetMovieDetail(imdbID: "tt1981115").request { [weak self] (outcome) in
+            guard let strongSelf = self else { return }
+            switch outcome
+            {
+            case .success(let moviesDetail):
+                if let movieDetail = moviesDetail.first
+                {
+                    strongSelf.setUpScreenElements(with: movieDetail)
+                }
+                else
+                {
+                    strongSelf.showErrorAlert()
+                }
+            case .failure:
+                strongSelf.showErrorAlert()
+            }
+        }
+    }
+
+    private func showErrorAlert()
+    {
+        let alertController = UIAlertController(title: "Error", message: "There was a problem loading the movie information.", preferredStyle: .alert)
+        let okayAction = UIAlertAction(title: "okay", style: .default) { _ in
+            self.navigationController?.popViewController(animated: true)
+        }
+        alertController.addAction(okayAction)
+        present(alertController, animated: true)
+    }
+
+    private func setUpScreenElements(with movieDetail: MovieDetail)
+    {
+        loadingIndicator.stopAnimating()
+        screenElements(shouldShow: true)
+        movieTitleLabel.text = movieDetail.title
+        movieRatingLabel.text = movieDetail.rating
+        moviePlotLabel.text = movieDetail.plot
+    }
+
+    private func screenElements(shouldShow: Bool)
+    {
+        moviePosterImageView.isHidden = !shouldShow
+        movieTitleLabel.isHidden = !shouldShow
+        movieRatingLabel.isHidden = !shouldShow
+        moviePlotLabel.isHidden = !shouldShow
     }
 }
